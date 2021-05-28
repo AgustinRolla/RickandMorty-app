c41ed7db1015fee8e9a3d1eef695382e15ad0a3b
Agustin Rolla <70298308+AgustinRolla@users.noreply.github.com>
2021-05-28 18:52:36 -0300

    favorites

--- src/pages/Favorites/favorites.js
+++ src/pages/Favorites/favorites.js
@@ -3,7 +3,5 @@ import React from "react";
 import { useHistory } from "react-router";
 
 export const Favorites = ({ favorites, deleteFavorite }) => {
-  const favorites = favorites.map((favorite) => `${favorite.data}`);
-
-  return "favoritos";
+  return favorites.map((favorite) => `${favorite.name}`);
 };

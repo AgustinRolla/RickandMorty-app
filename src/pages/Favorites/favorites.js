dee81563ec2ce4f58e819815f75a17f58b68a07d
Agustin Rolla <70298308+AgustinRolla@users.noreply.github.com>
2021-05-28 20:04:47 -0300

    funcion random

--- src/pages/Favorites/favorites.js
+++ src/pages/Favorites/favorites.js
@@ -1,7 +1,95 @@
-import styled from "styled-components";
 import React from "react";
+import styled from "styled-components";
 import { useHistory } from "react-router";
 
-export const Favorites = ({ favorites, deleteFavorite }) => {
-  return favorites.map((favorite) => `${favorite.name}`);
+export const Favorites = ({ favorites, setCharacter }) => {
+  const [input, setInput] = React.useState();
+
+  const history = useHistory();
+
+  function handleSearchClick() {
+    setCharacter(input.toLowerCase());
+    history.replace("./card");
+  }
+
+  function handleFavoriteClick() {
+    console.log("boton favorito");
+    history.replace("./favorites");
+  }
+
+  function handleInputChange(event) {
+    setInput(event.target.value);
+  }
+
+  const handleRandomClick = () => {
+    const randomCharacter = Math.floor(Math.random() * 671) + 1;
+    setCharacter(randomCharacter);
+    history.replace("./card");
+  };
+
+  return (
+    <Home>
+      <Wrapper>{favorites.map((favorite) => `${favorite.name}`)}</Wrapper>
+    </Home>
+  );
 };
+
+const Home = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: center;
+  background-image: url("https://i.redd.it/wx9mj52s8kly.jpg");
+  background-size: cover;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 9%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+`;
+
+const SearchBar = styled.input`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const Logo = styled.img`
+  width: 175%;
+  margin-bottom: 20px;
+`;
+const ButtonsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 20px;
+  margin-top: 5px;
+
+  button {
+    cursor: pointer;
+    color: black;
+    padding: 10px;
+    font-weight: 600;
+    background-color: #a6eee6ff;
+    border: none;
+    border-radius: 6px;
+    transition: all ease-out 0.3s;
+
+    &:hover {
+      filter: brightness(0.8);
+      transform: translateY(-4px);
+    }
+
+    &:first-child {
+      width: 70%;
+    }
+
+    &:last-child {
+      width: 30%;
+    }
+  }
+`;
